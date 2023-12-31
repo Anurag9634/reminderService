4d0227da433ad9194f6c9974b26ae9afd1618013
Anurag <anuragmathur41@gmail.com>
2023-12-31 15:58:13 +0530

    done basic setup and first Commit

---
+++ ReminderService/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {PORT}   = require('./config/serverConfig')
+
+const setupAndStartServer = ()=>{
+    const app = express();   
+    app.use(express.json());
+    app.use(express.urlencoded({ extended : true }));
+
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+
+}
+
+
+setupAndStartServer();
